0946e86c1b071c1e044d364f9b18e585fa168ea8
Igor Bogoslavskyi <igor.bogoslavskyi@gmail.com>
2017-01-23 18:30:32 +0100

    until there is a better solution on ubuntu 16.04 use generic laser calibration

--- examples/simple_nodes/show_objects_moosman.cpp
+++ examples/simple_nodes/show_objects_moosman.cpp
@@ -44,8 +44,8 @@ void ReadData(const Radians& angle_tollerance, const string& in_path,
   auto image_reader =
       FolderReader(in_path, ".png", FolderReader::Order::SORTED);
   auto config_reader = FolderReader(in_path, "img.cfg");
-  auto proj_params_ptr =
-      ProjectionParams::FromConfigFile(config_reader.GetNextFilePath());
+  auto proj_params_ptr = ProjectionParams::HDL_64();
+      // ProjectionParams::FromConfigFile(config_reader.GetNextFilePath());
 
   auto depth_ground_remover = DepthGroundRemover(
       *proj_params_ptr, ground_remove_angle, smooth_window_size);
